994105c1497786ee00f5f4f00f5617d8b556c74d
Christos Petropoulos <chris.petropoulos91@gmail.com>
2017-07-04 23:10:42 +0300

    refactor(releasing): Add travis config, conventional commit, semantic-release

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var module=require('./index');
+var expect=require('chai').expect;
+
+describe('workdates',function(){
+    it('should work!',function(){
+        expect(true).to.be.true;
+    })
+})
